6f3a8043af352587ff1de01a1c0b0b207015d03e
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-02-13 23:22:47 +0100

    feat(mobile): add tauri generated android project files

---
+++ src-tauri/gen/android/app/src/main/java/io/levysworks/fleetcore_desktop/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.levysworks.fleetcore_desktop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
